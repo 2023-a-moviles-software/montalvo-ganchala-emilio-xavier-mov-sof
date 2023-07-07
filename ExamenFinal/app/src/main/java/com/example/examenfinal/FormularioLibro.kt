2c8e984eae490b5588a9e4e6d3dc8d4d06033ba8
EmilioMontalvo <80224370+EmilioMontalvo@users.noreply.github.com>
2023-07-06 22:39:25 -0500

    vista

--- ExamenFinal/app/src/main/java/com/example/examenfinal/FormularioLibro.kt
+++ ExamenFinal/app/src/main/java/com/example/examenfinal/FormularioLibro.kt
@@ -1,12 +1,11 @@
-package com.example.examenfinal.ui.theme
+package com.example.examenfinal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.examenfinal.R
 
-class FormularioAutor : AppCompatActivity() {
+class FormularioLibro : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_formulario_autor)
+        setContentView(R.layout.activity_formulario_libro)
     }
 }
